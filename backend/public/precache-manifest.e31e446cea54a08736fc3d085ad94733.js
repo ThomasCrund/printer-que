a07e03abe4dfb885b5ea3b926d08a2a3386bfc6c
ThomasCrund <39323104+ThomasCrund@users.noreply.github.com>
2020-08-07 17:02:05 +1000

    Added backend

---
+++ backend/public/precache-manifest.e31e446cea54a08736fc3d085ad94733.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d3854bbdc4f1135a16b82973de85ec9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c87065b0f323bda2c28",
+    "url": "/static/css/main.388887c8.chunk.css"
+  },
+  {
+    "revision": "78f6393bad0b1dda36bf",
+    "url": "/static/js/2.0c836958.chunk.js"
+  },
+  {
+    "revision": "ef7f0128066dad638e14e7838f835a83",
+    "url": "/static/js/2.0c836958.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c87065b0f323bda2c28",
+    "url": "/static/js/main.6eb6ab47.chunk.js"
+  },
+  {
+    "revision": "dedf461a29a53fd0fe6e",
+    "url": "/static/js/runtime-main.a6c647a6.js"
+  }
+]);
